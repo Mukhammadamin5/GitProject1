7ea6327ea0d8370a6c5d95056e27b8b698595bb1
Oblakulov Mukhammadamin <moblakulov5@gmail.com>
2022-08-16 16:15:26 -0700

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -3,6 +3,8 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work started");
+        System.out.println("Created Git Repository");
+
     }
 
 }
